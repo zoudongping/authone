ad0af4394062178ff668602346241e26381ac2dd
ZDP <1010930325@qq.com>
2017-08-04 09:14:39 +0800

    改了下

---
+++ auth-dao/src/main/java/entity/Userinfo.java
@@ -0,0 +1,53 @@
+package entity;
+
+/**
+ * Created by 李晨曦 2017年08月03日
+ */
+public class Userinfo {
+    private int uid;
+    private String uname;
+    private String upass;
+    private String ustatus;
+
+    @Override
+    public String toString() {
+        return "Userinfo{" +
+                "uid=" + uid +
+                ", uname='" + uname + '\'' +
+                ", upass='" + upass + '\'' +
+                ", ustatus='" + ustatus + '\'' +
+                '}';
+    }
+
+    public String getUname() {
+        return uname;
+    }
+
+    public void setUname(String uname) {
+        this.uname = uname;
+    }
+
+    public int getUid() {
+        return uid;
+    }
+
+    public void setUid(int uid) {
+        this.uid = uid;
+    }
+
+    public String getUpass() {
+        return upass;
+    }
+
+    public void setUpass(String upass) {
+        this.upass = upass;
+    }
+
+    public String getUstatus() {
+        return ustatus;
+    }
+
+    public void setUstatus(String ustatus) {
+        this.ustatus = ustatus;
+    }
+}
